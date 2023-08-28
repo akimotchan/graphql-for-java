3853a14644a7b39e9ec2b6575e4e67049047f1c7
akimotchan <makimoto20011522@gmail.com>
2023-08-28 23:40:49 +0900

    #6 RV指摘を修正

--- src/test/java/com/example/demo/TaskControllerTests.java
+++ src/test/java/com/example/demo/TaskControllerTests.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.graphql.test.tester.GraphQlTester;
 
 @GraphQlTest(TaskController.class)
